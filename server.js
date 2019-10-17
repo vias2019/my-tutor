0da395b261af591ad33e5f7beea0424727806494
Victoria Grishkina <vias2010@gmail.com>
2019-10-17 00:29:22 -0700

    Added additional boiler plate files

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
-var mongoose = require("mongoose");
+// var mongoose = require("mongoose");
 
-var model = require("./model.js");
+// var model = require("./model.js");
 
-mongoose.connect("mongodb://localhost/maindatabase", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/maindatabase", { useNewUrlParser: true });
 
 
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Define API routes here
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
