e1547ccdeadd62b1e8431df97febd42b80e663a2
Stephanie Hughes <stephaniegracehughes@gmail.com>
2019-10-19 11:26:50 -0700

    invite student to register modal/button component

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,24 @@
-import React from 'react';
+import React, { Component } from "react";
 import './App.css';
+import RegistrInvite from './Components/RegistrInvite';
 
-//import RegistrPage from "./Components/RegistrInvite";
 //import StudentPay from "./Components/StudentPay";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+         
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
 
-    </div>
-
-
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
