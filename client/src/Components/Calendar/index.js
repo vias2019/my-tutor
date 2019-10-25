743c021ef899edba9e47d6ba226c8a19013b9bf8
SilverBell17 <kevinjoun17@gmail.com>
2019-10-24 20:14:55 -0700

    side branch for messing with calendar object and design

--- client/src/Components/Calendar/index.js
+++ client/src/Components/Calendar/index.js
@@ -17,11 +17,15 @@ function Calendar(){
             center: "title",
             right: "dayGridMonth,timeGridWeek,listWeek",
           }}
+          height={'auto'}
           plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
           //to get events to show up, create a separate events object and use props to pass in the event details. Events takes in an array of object. Each object in the array should have the title of the event with a data following this format: 2019-10-20T08:00. Default duration of all events is 1 hour.
+          minTime='09:00:00'
+          maxTime='20:00:00'
           events={[{
             title: "Student Name@@",
-            date: "2019-10-20T08:00",
+            date: "2019-10-20T09:00",
+            duration: '04:00:00'
           },
           {
             title: "Student Name2",
