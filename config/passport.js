e8e119adfe4d49c635fca5a13b39be52765d41c1
Tim Holeski <trholeski@gmail.com>
2019-11-03 11:56:02 -0800

    updating password works

--- config/passport.js
+++ config/passport.js
@@ -2,32 +2,32 @@ const bcrypt = require("bcryptjs");
 // const bcrypt_SALT_ROUNDS = 12;
 
 const passport = require('passport'),
-  localStrategy = require('passport-local').Strategy,
-  db = require('../model'),
-  JWTstrategy = require('passport-jwt').Strategy,
-  ExtractJWT = require('passport-jwt').ExtractJwt;
+localStrategy = require('passport-local').Strategy,
+db = require('../model'),
+JWTstrategy = require('passport-jwt').Strategy,
+ExtractJWT = require('passport-jwt').ExtractJwt;
 
-  // called on login, saves the id to session req.session.passport.user = {id:'..'}
+// called on login, saves the id to session req.session.passport.user = {id:'..'}
 passport.serializeUser((user, done) => {
-	console.log('*** serializeUser called, user: ')
-	console.log(user) // the whole raw user object!
-	console.log('---------')
-	done(null, { _id: user._id })
+console.log('*** serializeUser called, user: ')
+console.log(user) // the whole raw user object!
+console.log('---------')
+done(null, { _id: user._id })
 })
 
 // user object attaches to the request as req.user
 passport.deserializeUser((id, done) => {
-	console.log('DeserializeUser called')
-	User.findOne(
-		{ _id: id },
-		'username',
-		(err, user) => {
-			console.log('*** Deserialize user, user:')
-			console.log(user)
-			console.log('--------------')
-			done(null, user)
-		}
-	)
+console.log('DeserializeUser called')
+User.findOne(
+{ _id: id },
+'username',
+(err, user) => {
+console.log('*** Deserialize user, user:')
+console.log(user)
+console.log('--------------')
+done(null, user)
+}
+)
 })
 
 
@@ -36,148 +36,153 @@ passport.deserializeUser((id, done) => {
 
 // Generates hash using bCrypt
 var createHash = function(password){
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(12), null);
-   }
+    var hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(12), null);
+    console.log('hashed password:', hashedPassword);
+    return hashedPassword;
+}
 
 
 passport.use(
-    'registerTeacher',
-    new localStrategy(
-      {
-        usernameField: 'emailid',
-        passwordField: 'password',
-        session: false,
-      },
-      (emailid, password, done) => {
-        try {
-          db.findOne({
-              emailid: emailid,
-          }).then(user => {
-            if (user != null) {
-              console.log('email address already taken');
-              return done(null, false, { message: 'emailid already taken' });
-            } else {
-                db.create({ 
-                  emailid, 
-                  password: createHash(password),
-                  isRegistered: true,
-                  isTeacher:true
-              }).then(user => {
-                  console.log('user created', user);
-                  // note the return needed with passport local - remove this return for passport JWT to work
-                  return done(null, user);
-                });
-            }
-          });
-        } catch (err) {
-          done(err);
-        }
-      },
-    ),
-  );
+'registerTeacher',
+new localStrategy(
+{
+usernameField: 'emailid',
+passwordField: 'password',
+session: false,
+},
+(emailid, password, done) => {
+try {
+db.findOne({
+emailid: emailid,
+}).then(user => {
+if (user != null) {
+console.log('email address already taken');
+return done(null, false, { message: 'emailid already taken' });
+} else {
+db.create({ 
+    emailid, 
+    password: createHash(password),
+    isRegistered: true,
+    isTeacher:true
+}).then(user => {
+    console.log('user created', user);
+    // note the return needed with passport local - remove this return for passport JWT to work
+    return done(null, user);
+});
+}
+});
+} catch (err) {
+done(err);
+}
+},
+),
+);
 
 
-  passport.use(
+passport.use(
     'registerStudent',
     new localStrategy(
-      {
+        {
         usernameField: 'emailid',
         passwordField: 'password',
         session: false,
-      },
-      (emailid, password, done) => {
-        try {
-          db.findOne({
-              emailid: emailid,
-          }).then(user => {
-            if (user != null) {
-              console.log('email address already taken');
-              return done(null, false, { message: 'emailid already taken' });
-            } else {
-                db.create({ 
-                  emailid, 
-                  password: createHash(password),
-                  isRegistered: true,
-                  isTeacher:false
-              }).then(user => {
-                  console.log('user created', user);
-                  // note the return needed with passport local - remove this return for passport JWT to work
-                  return done(null, user);
+        },
+        (emailid, password, done) => {
+        //try nested find queries
+            try {
+                db.find({
+                    emailid: emailid,
+                }).then(user => {
+                    if (user != null) {
+                        if(user.isRegistered){
+                            return done(null, false, { message: 'A student has already registered with this email address' });
+                        } else {
+                            var hashedPassword = createHash(password).toString();
+                            console.log('found the updated password', hashedPassword);
+                            db.findOneAndUpdate(
+                                { emailid: emailid },
+                                { password: hashedPassword }, 
+                                { new: true  }
+                                ).then(updatedUser => {
+                                console.log('updatedUser created', updatedUser);
+                                // note the return needed with passport local - remove this return for passport JWT to work
+                                return done(null, updatedUser);
+                            });
+                        }
+                    }
                 });
+            } catch (err) {
+                done(err);
             }
-          });
-        } catch (err) {
-          done(err);
-        }
-      },
+        },
     ),
-  );
+);
 
 passport.use(
-  'login',
-  new localStrategy(
-    {
-      usernameField: 'emailid',
-      passwordField: 'password',
-      session: false,
-    },
-    (emailid, password, done) => {
-      try {
-        db.findOne({
-            emailid: emailid,
-        }).then(user => {
-          if (user === null) {
-            return done(null, false, { message: 'bad email address' });
-          } else {
-            console.log('in passport showing user: ', user);
-            console.log('in passport showing password: ', password);
-            const passwordsMatch = bcrypt.compareSync(password, user.password);//.then(response => {
-                
-              if (!passwordsMatch) {
-                console.log('passwords do not match');
-                return done(null, false, { message: 'passwords do not match' });
-              }
-              console.log('user found & authenticated');
-              
-              // note the return needed with passport local - remove this return for passport JWT
-              return done(null, user);
-            // });
-          }
-        });
-      } catch (err) {
-        done(err);
-      }
-    },
-  ),
+'login',
+new localStrategy(
+{
+usernameField: 'emailid',
+passwordField: 'password',
+session: false,
+},
+(emailid, password, done) => {
+try {
+db.findOne({
+emailid: emailid,
+}).then(user => {
+if (user === null) {
+return done(null, false, { message: 'bad email address' });
+} else {
+console.log('in passport showing user: ', user);
+console.log('in passport showing password: ', password);
+const passwordsMatch = bcrypt.compareSync(password, user.password);//.then(response => {
+
+if (!passwordsMatch) {
+console.log('passwords do not match');
+return done(null, false, { message: 'passwords do not match' });
+}
+console.log('user found & authenticated');
+
+// note the return needed with passport local - remove this return for passport JWT
+return done(null, user);
+// });
+}
+});
+} catch (err) {
+done(err);
+}
+},
+),
 );
 
 const opts = {
-  jwtFromRequest: ExtractJWT.fromAuthHeaderWithScheme('JWT'),
-  secretOrKey: process.env.TOKEN, // get jwt secret from environment variable,
+jwtFromRequest: ExtractJWT.fromAuthHeaderWithScheme('JWT'),
+secretOrKey: process.env.TOKEN, // get jwt secret from environment variable,
 };
 
 passport.use(
-  'jwt',
-  new JWTstrategy(opts, (jwt_payload, done) => {
-    try {
-      db.findOne({
-        where: {
-          emailid: jwt_payload.id,
-        },
-      }).then(user => {
-        if (user) {
-          console.log('user found in db in passport');
-          // note the return removed with passport JWT - add this return for passport local
-          done(null, user);
-        } else {
-          console.log('user not found in db');
-          done(null, false);
-        }
-      });
-    } catch (err) {
-      done(err);
-    }
-  }),
+'jwt',
+new JWTstrategy(opts, (jwt_payload, done) => {
+try {
+db.findOne({
+where: {
+emailid: jwt_payload.id,
+},
+}).then(user => {
+if (user) {
+console.log('user found in db in passport');
+// note the return removed with passport JWT - add this return for passport local
+done(null, user);
+} else {
+console.log('user not found in db');
+done(null, false);
+}
+});
+} catch (err) {
+done(err);
+}
+}),
 );
 
 module.exports = passport
--- controllers/studentAuthRoute.js
+++ controllers/studentAuthRoute.js
@@ -2,40 +2,67 @@ const db = require("../model");
 const passport = require('passport');
 
 module.exports = app => {
-  app.post('/student-registration', (req, res, next) => {
-    passport.authenticate('registerStudent', (err, user, info) => {
-      if (err) {
-        console.log(err);
-      }
-      if (info != undefined) {
-        console.log(info.message);
-        res.send(info.message);
-      } else {
-          console.log('req.body: ', req.body);
-          console.log('user: ', user);
-          
-        req.logIn(user, err => {
-          const data = {
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            password: user.password,
-            emailid: user.emailid
-          };
-          db.findOneAndUpdate({
-            emailid: data.emailid,
-        },{
-          firstName: data.firstName,
-          lastName: data.lastName,
-          password: data.password,
-          isTeacher:false
-        },
-        {new: true}).then(updatedUser => {
-            console.log('updateUser: ', updatedUser);
-            console.log('user created in db');
-            res.status(200).send({ message: 'user created' });
+    app.post('/student-registration', (req, res, next) => {
+        var wasInvited = false;
+        var passwordsMatch = false;
+        var reqBody = req.body;
+        console.log('request body: ', reqBody);
+
+            db.findOne({
+                emailid: reqBody.emailid,
+            }).then(user => {
+                console.log('user object after findOne: ', user);
+                if (user != null) {
+                    wasInvited = true;
+                    if (req.body.password === req.body.confirmpassword){
+                        passwordsMatch = true;
+                    }
+                }
+                else{
+                    console.log('student does not exist');
+                    wasInvited = false;
+                };
+            
+                if (wasInvited && passwordsMatch){
+                    passport.authenticate('registerStudent', (err, user, info) => {
+                        if (err) {
+                            console.log(err);
+                        }
+                        if (info != undefined) {
+                            console.log(info.message);
+                            res.send(info.message);
+                        } else {
+                            req.logIn(user, err => {
+                                const data = {
+                                    firstName: req.body.firstName,
+                                    lastName: req.body.lastName,
+                                    password: user[0].password,
+                                    emailid: user[0].emailid,
+                                    isRegistered: true
+                                };
+                                console.log('req.user: ', req.user);
+                                console.log('user in auth route: ', user);
+                                console.log('email address in data object in auth route: ', data.emailid);
+                                db.findOneAndUpdate({
+                                emailid: data.emailid,
+                                },
+                                {
+                                firstName: data.firstName,
+                                lastName: data.lastName,
+                                password: data.password,
+                                isTeacher:false,
+                                isRegistered: data.isRegistered
+                                },
+                                {new: true}).then(routeUpdatedUser => {
+                                    console.log('routeUpdateUser: ', routeUpdatedUser);
+                                    console.log('user created in db');
+                                    res.status(200).send({ message: 'user created' });
+                                });
+                            });
+                        }
+                    })(req, res, next);
+                }
             });
-        });
-      }
-    })(req, res, next);
-  });
+    });
+
 };
