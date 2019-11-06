63a5a7496c90537a4a0b5d733ac62b84e9425b12
Victoria Grishkina <vias2010@gmail.com>
2019-11-06 00:28:15 -0800

    Manifest

---
+++ client/build/precache-manifest.b627d0a93df510a32d1f45ee4306d8d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "215acd0e3b0d38b98531af9e3b9b4fb9",
+    "url": "/static/media/owllogo.215acd0e.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "245e604b112e5faa25f4",
+    "url": "/static/js/main.503215d5.chunk.js"
+  },
+  {
+    "revision": "09ab591342122004dd31",
+    "url": "/static/js/2.b73921e0.chunk.js"
+  },
+  {
+    "revision": "245e604b112e5faa25f4",
+    "url": "/static/css/main.e5ef34fd.chunk.css"
+  },
+  {
+    "revision": "09ab591342122004dd31",
+    "url": "/static/css/2.f0d9214f.chunk.css"
+  },
+  {
+    "revision": "614ed30e8ff045359796e2348f77cf08",
+    "url": "/index.html"
+  }
+];
