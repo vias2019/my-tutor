d3f2378382e538e50af950c63fa541c2e55b7c42
vias2019 <48966517+vias2019@users.noreply.github.com>
2019-11-06 23:38:46 -0800

    Update server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ require('./controllers/logoutAuthRoute')(app);
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.listen(PORT, () => {
